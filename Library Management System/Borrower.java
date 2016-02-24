9d40e9c742775c94908c464a89776ca8c38d893f
weixinTan <Tanw@bxscience.edu>
2016-02-23 19:57:24 -0500

    Update Borrower.java

--- Library Management System/Borrower.java
+++ Library Management System/Borrower.java
@@ -1,3 +1,12 @@
 public abstract class Borrower{
+  int ISBN;
+  String BookName,category;
+  public Borrower(int number,String Name, String Category){
+    ISBN= number;
+    BookName=name;
+    category=Category;
+  }
+  
+  abstract void PrintReceipt(String name，String Category, int number ){}
   
 }
